f0577f06c646451fc81b7f966dafc070761c47bd
David G Chaves <davidgchaves@gmail.com>
2014-06-23 19:08:02 +0200

    spec: initializes the 'Config' property in the 'App' object

--- test/js/spec/namespace.spec.js
+++ test/js/spec/namespace.spec.js
@@ -2,4 +2,8 @@ describe("Namespace", function () {
   it("provides the 'App' object", function () {
     expect(App).to.be.an("object");
   });
+
+  it("initializes the 'Config' property in the 'App' object", function () {
+    expect(App).to.include.key("Config");
+  });
 });
