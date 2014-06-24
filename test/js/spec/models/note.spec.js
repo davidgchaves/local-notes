37415be920a417c9d8e0d667608b331993c52e34
David G Chaves <davidgchaves@gmail.com>
2014-06-24 18:26:26 +0200

    spec: App.Models.Note has default values

--- test/js/spec/models/note.spec.js
+++ test/js/spec/models/note.spec.js
@@ -3,4 +3,11 @@ describe("App.Models.Note", function () {
     var note = new App.Models.Note();
     expect(note).to.be.ok;
   });
+
+  it("has default values", function () {
+    var note = new App.Models.Note();
+    expect(note.get("title")).to.equal("");
+    expect(note.get("text")).to.equal("*Edit your note!*");
+    expect(note.get("createdAt")).to.be.a("date");
+  });
 });
