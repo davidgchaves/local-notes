a99596b537b2c0335df2e34955a66aea61dfe939
David G Chaves <davidgchaves@gmail.com>
2014-06-24 20:33:16 +0200

    Dry and refactor specs to better revealing intent

--- test/js/spec/collections/notes.spec.js
+++ test/js/spec/collections/notes.spec.js
@@ -2,16 +2,20 @@ describe("App.Collections.Notes", function () {
 
   describe("an empty notes collection", function () {
 
-    it("is a valid collection", function () {
+    function createEmptyNotesCollectionAndClearLocalStorage () {
       var notes = new App.Collections.Notes();
       notes.localStorage._clear();
+      return notes;
+    }
+
+    it("is a valid collection", function () {
+      var notes = createEmptyNotesCollectionAndClearLocalStorage();
 
       expect(notes).to.be.ok;
     });
 
     it("has no notes in it", function () {
-      var notes = new App.Collections.Notes();
-      notes.localStorage._clear();
+      var notes = createEmptyNotesCollectionAndClearLocalStorage();
 
       expect(notes).to.have.length(0);
     });
