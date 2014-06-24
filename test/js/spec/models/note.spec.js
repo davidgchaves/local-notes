6746e53ccd5d24682020bec63ba3b15d21fb5e5e
David G Chaves <davidgchaves@gmail.com>
2014-06-24 18:32:08 +0200

    spec: App.Models.Note sets passed attributes

--- test/js/spec/models/note.spec.js
+++ test/js/spec/models/note.spec.js
@@ -1,13 +1,25 @@
 describe("App.Models.Note", function () {
   it("is a valid model", function () {
     var note = new App.Models.Note();
+
     expect(note).to.be.ok;
   });
 
   it("has default values", function () {
     var note = new App.Models.Note();
+
     expect(note.get("title")).to.equal("");
     expect(note.get("text")).to.equal("*Edit your note!*");
     expect(note.get("createdAt")).to.be.a("date");
+  });
+
+  it("sets passed attributes", function () {
+    var note = new App.Models.Note({
+      title: "Grocery List",
+      text: "Milk\n Coffee\n Cookies"
+    });
+
+    expect(note.get("title")).to.equal("Grocery List");
+    expect(note.get("text")).to.equal("Milk\n Coffee\n Cookies");
   });
 });
