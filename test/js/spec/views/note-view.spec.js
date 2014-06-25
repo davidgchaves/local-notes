0004745dc9b0cc438cc3a754e5692966cc0acd06
David G Chaves <davidgchaves@gmail.com>
2014-06-25 12:25:54 +0200

    spec: App.Views.NoteView can render a fairly complicated note   Some refactoring to try to ease the pain...   ... but the pain remains.   Check the last commit comments. THAT!

--- test/js/spec/views/note-view.spec.js
+++ test/js/spec/views/note-view.spec.js
@@ -5,22 +5,36 @@ describe("App.Views.NoteView", function () {
     that.$fixture.empty().appendTo($("#fixtures"));
   }
 
-  function createViewToTriggerEmtpyNoteRendering(that) {
-    that.viewForEmptyNote = new App.Views.NoteView({
+  function createViewToTriggerNoteRendering(that) {
+    that.viewForNote = new App.Views.NoteView({
       el: that.$fixture,
       model: new App.Models.Note()
     });
   }
 
   function destroyTheNoteToAlsoDestroyTheView(that) {
-    that.viewForEmptyNote.model.destroy();
+    that.viewForNote.model.destroy();
   }
+
+  function updateModelToTriggerNoteRendering(that) {
+    that.viewForNote.model.set({
+      title: "Test Note Title",
+      text: "## Test Note Heading\n" + "* List item 1\n" + "* List item 2"
+    });
+  }
+
+  beforeEach(function () {
+    createAndSetupTheTestFixture(this);
+  });
+
+  afterEach(function () {
+    destroyTheNoteToAlsoDestroyTheView(this);
+  });
 
   it("can render an empty note", function () {
     var $noteTitle, $noteText;
 
-    createAndSetupTheTestFixture(this);
-    createViewToTriggerEmtpyNoteRendering(this);
+    createViewToTriggerNoteRendering(this);
 
     $noteTitle = $("#pane-title");
     $noteText = $("#pane-text");
@@ -30,7 +44,27 @@ describe("App.Views.NoteView", function () {
 
     expect($noteText.text()).to.equal("Edit your note!");
     expect($noteText.html()).to.contain("<p><em>Edit your note!</em></p>");
+  });
 
-    destroyTheNoteToAlsoDestroyTheView(this);
+  it("can render a fairly complicated note", function (done) {
+    var $noteTitle, $noteText;
+
+    createViewToTriggerNoteRendering(this);
+
+    this.viewForNote.model.once("change", function () {
+      $noteTitle = $("#pane-title");
+      $noteText = $("#pane-text");
+
+      expect($noteTitle.text()).to.equal("Test Note Title");
+
+      expect($noteText.html())
+        .to.contain("Test Note Heading</h2>").and
+        .to.contain("<ul>").and
+        .to.contain("<li>List item 2</li>");
+
+      done();
+    });
+
+    updateModelToTriggerNoteRendering(this);
   });
 });
