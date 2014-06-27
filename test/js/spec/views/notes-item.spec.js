1941c2d9a9fc939ef3c4c0ef543a2c704f141f0b
David G Chaves <davidgchaves@gmail.com>
2014-06-27 13:10:16 +0200

    spec: remove is invoked on model destroy event

---
+++ test/js/spec/views/notes-item.spec.js
@@ -0,0 +1,35 @@
+describe("App.Views.NotesItem", function () {
+
+  function createStubRouter() { return sinon.stub(); }
+
+  function createRealNote() { return new App.Models.Note({ id: "0", title: "title" }); }
+
+  function createViewNotesItemWith(that, realNote, stubRouterNavigation) {
+    that.view = new App.Views.NotesItem({ model: realNote },
+                                        { router: { navigate: stubRouterNavigation } });
+  }
+
+  function removeViewNotesItem(that) {
+    that.view.remove();
+  }
+
+  describe("remove", function () {
+
+    function preventViewFromActuallyBeenRemovedFromTestEnviromentWhenCalled(that) {
+      that.stub(that.view, "remove");
+    }
+
+    it("is invoked on model destroy event", sinon.test(function () {
+      var stubRouter = createStubRouter();
+      var realNote = createRealNote();
+      createViewNotesItemWith(this, realNote, stubRouter);
+      preventViewFromActuallyBeenRemovedFromTestEnviromentWhenCalled(this);
+
+      this.view.model.trigger("destroy");
+
+      expect(this.view.remove).to.have.been.calledOnce;
+
+      removeViewNotesItem(this);
+    }));
+  });
+});
